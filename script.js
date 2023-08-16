d3235b6525bbc52612126e8c5640e2e7162207e0
AndreyLitvinovTop <121670163+AndreyLitvinovTop@users.noreply.github.com>
2023-08-16 21:23:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function open_panel(){
+   var panel = document.getElementById('panel');
+   panel.style.display = "block";
+}
+
+function close_panel(){
+   var panel = document.getElementById('panel');
+   panel.style.display = "none";
+}
